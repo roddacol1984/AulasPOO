dd34d966a1a87e597320326986b1ca182ac3a15b
Rodrigo Da Col <roddacol@gmail.com>
2020-05-06 23:22:07 -0300

    Alterado classe Box e TesteBoxT

--- POO_USCS/src/ativ8/Box.java
+++ POO_USCS/src/ativ8/Box.java
@@ -1,15 +1,15 @@
 package ativ8;
 
 
-public class Box<T> {
+public class Box<S,T> {
 
-	private T item1;
+	private S item1;
 	private T item2;
 	
-	public T getItem1() {
+	public S getItem1() {
 		return item1;
 	}
-	public void setItem1(T item1) {
+	public void setItem1(S item1) {
 		this.item1 = item1;
 	}
 	public T getItem2() {
@@ -23,7 +23,7 @@ public class Box<T> {
 		
 	}
 	
-	public Box(T item1, T item2) {
+	public Box(S item1, T item2) {
 		this.item1 = item1;
 		this.item2 = item2;
 	}
@@ -31,7 +31,7 @@ public class Box<T> {
 	public void imprimeBox () {
 		System.out.println(this.item1 + " / " + this.item2);
 	}
-	public void alteraItem1( T item) {
+	public void alteraItem1( S item) {
 		this.item1 = item;
 		
 	}
--- POO_USCS/src/ativ8/TesteBoxT.java
+++ POO_USCS/src/ativ8/TesteBoxT.java
@@ -5,21 +5,18 @@ public class TesteBoxT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Box<Integer> a = new Box<Integer>(20,40);
+		Box<String, Integer> a = new Box<String, Integer>("Rodrigo", 40);
 		a.imprimeBox();
-		a.alteraItem1(50);
+		a.alteraItem1("Aimée");
 		a.alteraItem2(40);
 		a.imprimeBox();
-		
-		Box<String> b = new Box<String>("Rodrigo", "Aimée");
+
+		Box<Integer, String> b = new Box<Integer, String>(45, "Aimée");
 		b.imprimeBox();
-		b.alteraItem1("Aimée");
+		b.alteraItem1(334);
 		b.alteraItem2("Rodrigo");
 		b.imprimeBox();
-		
-		
-		
-		
+
 	}
 
 }
